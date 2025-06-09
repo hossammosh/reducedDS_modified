96de4a2e4d1f03b66ac3583464a946da528e5fcb
hossammosh <37549426+hossammosh@users.noreply.github.com>
2025-06-09 20:41:20 +0300

    try 4 which is also good

--- lib/train/data_recorder.py
+++ lib/train/data_recorder.py
@@ -13,7 +13,7 @@ from openpyxl.utils import get_column_letter
 import copy
 
 # --- Configuration ---
-_chunk_size = 5  # Save every 10,000 samples
+_chunk_size = 20  # Save every 10,000 samples
 _delete_chunks_after_merge = True  # Set to False to keep intermediate chunk files
 
 # --- Global State (Protected by Lock) ---
